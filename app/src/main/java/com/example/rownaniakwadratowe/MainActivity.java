3923dd38b4c2cb5818a9f14c1cb83b65ec127229
JediSebas <sebas123456665@gmail.com>
2020-11-09 14:30:42 +0100

    Imaginary numbers

--- app/src/main/java/com/example/rownaniakwadratowe/MainActivity.java
+++ app/src/main/java/com/example/rownaniakwadratowe/MainActivity.java
@@ -49,7 +49,7 @@ public class MainActivity extends AppCompatActivity {
         try{
             bs = paramBet.getText().toString().trim();
             b = Double.parseDouble(bs);
-        } catch (Exception e) { paramBet.setText("0"); }
+        } catch (NumberFormatException e) { paramBet.setText("0"); }
         try{
             cs = paramCet.getText().toString().trim();
             c = Double.parseDouble(cs);
@@ -61,51 +61,71 @@ public class MainActivity extends AppCompatActivity {
                 if (delta > 0) {
                     x1 = (-b + sqrt(delta)) / (2 * a);
                     x2 = (-b - sqrt(delta)) / (2 * a);
-                    x1 *= 100;
-                    x2 *= 100;
+                    x1 *= 1000;
+                    x2 *= 1000;
                     x1 = Math.round(x1);
                     x2 = Math.round(x2);
-                    x1 /= 100;
-                    x2 /= 100;
+                    x1 /= 1000;
+                    x2 /= 1000;
                     String wyn1 = String.valueOf(x1);
                     String wyn2 = String.valueOf(x2);
                     wynik1et.setText(wyn1);
                     wynik2et.setText(wyn2);
                 } else if (delta == 0) {
                     x1 = (-b) / (2 * a);
-                    x1 *= 100;
+                    x1 *= 1000;
                     x1 = Math.round(x1);
-                    x1 /= 100;
+                    x1 /= 1000;
                     String wyn1 = String.valueOf(x1);
                     wynik1et.setText(wyn1);
                     wynik2et.setText("Brak");
                 } else if (delta < 0) {
-                    wynik1et.setText("Liczba nierzeczywista");
-                    wynik2et.setText("Liczba nierzeczywista");
+                    double cz1 = -b / 2*a;
+                    double cz2 = sqrt(-delta)/ 2*a;
+                    cz1 *= 1000;
+                    cz2 *= 1000;
+                    cz1 = Math.round(cz1);
+                    cz2 = Math.round(cz2);
+                    cz1 /= 1000;
+                    cz2 /= 1000;
+                    String cz1s = String.valueOf(cz1);
+                    String cz2s = String.valueOf(cz2);
+                    wynik1et.setText(cz1s + " + " + cz2s + "i");
+                    wynik2et.setText(cz1s + " - " + cz2s + "i");
                 }
             } else if (b == 0 && a != 0 && c != 0) {
                 if ((c < 0 || a < 0) && !(c < 0 && a < 0)) {
                     x1 = sqrt(-(c / a));
                     x2 = -(sqrt(-(c / a)));
-                    x1 *= 100;
-                    x2 *= 100;
+                    x1 *= 1000;
+                    x2 *= 1000;
                     x1 = Math.round(x1);
                     x2 = Math.round(x2);
-                    x1 /= 100;
-                    x2 /= 100;
+                    x1 /= 1000;
+                    x2 /= 1000;
                     String wyn1 = String.valueOf(x1);
                     String wyn2 = String.valueOf(x2);
                     wynik1et.setText(wyn1);
                     wynik2et.setText(wyn2);
                 } else if ((c < 0 && a < 0) || (c > 0 && a > 0)) {
-                    wynik1et.setText("Licba nierzeczywista");
-                    wynik2et.setText("Licba nierzeczywista");
+                    double cz1 = sqrt(c/a);
+                    double cz2 = -sqrt(c/a);
+                    cz1 *= 1000;
+                    cz2 *= 1000;
+                    cz1 = Math.round(cz1);
+                    cz2 = Math.round(cz2);
+                    cz1 /= 1000;
+                    cz2 /= 1000;
+                    String cz1s = String.valueOf(cz1);
+                    String cz2s = String.valueOf(cz2);
+                    wynik1et.setText(cz1s + "i");
+                    wynik2et.setText(cz2s + "i");
                 }
             } else if (c == 0 && a != 0 && b != 0) {
                 x2 = -b / a;
-                x2 *= 100;
+                x2 *= 1000;
                 x2 = Math.round(x2);
-                x2 /= 100;
+                x2 /= 1000;
                 String wyn2 = String.valueOf(x2);
                 wynik1et.setText("0");
                 wynik2et.setText(wyn2);
@@ -123,9 +143,9 @@ public class MainActivity extends AppCompatActivity {
                 wynik2et.setText("Brak");
             } else if (a == 0 && b != 0 && c != 0) {
                 x1 = -(c / b);
-                x1 *= 100;
+                x1 *= 1000;
                 x1 = Math.round(x1);
-                x1 /= 100;
+                x1 /= 1000;
                 String wyn1 = String.valueOf(x1);
                 wynik1et.setText(wyn1);
                 wynik2et.setText("Brak");
